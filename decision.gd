b0b2198d4e23928cd6d4994ca0df30f9967340c3
Pascal <pascal.duvaud@gmail.com>
2026-05-04 21:27:10 +0200

    Dissolve Finale eingebaut

--- decision.gd
+++ decision.gd
@@ -8,7 +8,6 @@ func choose_plant():
 	state = "life"
 	get_node("../Plant").interact()
 	apply_world_change()
-	print("Fuchs reagiert: " + state)
 	get_node("../Fox").react(state)
 
 func choose_tree():
@@ -17,18 +16,15 @@ func choose_tree():
 	state = "stability"
 	get_node("../Tree").interact()
 	apply_world_change()
-	print("Fuchs reagiert: " + state)
 	get_node("../Fox").react(state)
 
 func apply_world_change():
 	var env = get_node("../WorldEnvironment")
 	if env == null:
-		print("WorldEnvironment nicht gefunden!")
 		return
 	if state == "life":
-		print("Welt wird grüner")
 		env.environment.adjustment_saturation = 1.5
 	else:
-		print("Welt wird grauer")
 		env.environment.adjustment_saturation = 0.3
-	
+	await get_tree().create_timer(5.0).timeout
+	get_node("../Player").dissolve()
